12e7e2ce1e0c5fd8d5123170e6926b288a96aa02
James <J.Lemons2@gmail.com>
2018-11-17 14:23:26 -0500

    upate4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Omdb from "./components/omdb";
 import Header from "./components/Header";
 import Project from "./components/project1";
+import Selections from "./components/selections";
 import "./App.css";
 const App = () => 
 <main>
 <Header/>
 <Omdb />
+<Selections/>
 <Project/>
 </main>
 
--- src/components/selections/selections.js
+++ src/components/selections/selections.js
@@ -1,9 +1,67 @@
 import React from "react";
 import "./selections.css"
 
-const selections = () => (
-    <div id="name">
-      Cotton Swabs
-    </div>
-);
-export default selections;
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import friends from "../friends.json";
+
+// state ={
+//     friends
+//   }
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
+
+function selections(props) {
+  const { classes } = props;
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+      {this.state.friends.map(friend => (
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+    ))}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+selections.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(selections);
+
+// export default selections;
